d4bee98eab4511f449a178d19dd2f40dd7a8463b
chaitanyap97 <chaitanyapujari97@gmail.com>
2016-01-20 10:29:36 +0530

    Test Guru 99

---
+++ test99/src/test/java/guru/test99/AppTest.java
@@ -0,0 +1,34 @@
+package guru.test99;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	protected WebDriver driver;
+	@Test
+  public void guru99tutorials() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+		        "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+        String eTitle = "Meet Guru99";
+        String aTitle = "";
+        // launch Firefox and redirect it to the Base URL
+        driver.get("http://www.guru99.com/");
+        //maximises the browser window
+        driver.manage().window().maximize();
+        // get the actual value of the title
+        aTitle = driver.getTitle();
+        // compare the actual title  with the expected title 
+        if (aTitle.contentEquals(eTitle)){
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }        
+        //close Firefox browser
+        driver.close();
+		
+	}
+}
+
